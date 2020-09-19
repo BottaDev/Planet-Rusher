bb11112031b85ce620c31664328fbfe3b5bf8901
BottaDev <fedebotta@gmail.com>
2020-09-19 15:42:05 -0300

    Added Main Menu

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    // Called by the UI buttons
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Called by the exit button
+    public void ExiteGame()
+    {
+        Application.Quit();
+    }
+}
