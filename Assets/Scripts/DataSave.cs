a0774db8e9e157c24809b97781c6cf5dba54a860
BottaDev <fedebotta@gmail.com>
2021-02-10 17:16:23 -0300

    Main Menu Upgrade
    - Added Level 1 menu model
    - Added Level 2 menu model
    - Added Level 3 menu model

--- Assets/Scripts/DataSave.cs
+++ Assets/Scripts/DataSave.cs
@@ -8,6 +8,7 @@ public class DataSave
     public bool level2Completed = false;
     public bool level3Completed = false;
     public bool level4Completed = false;
+    public bool level5Completed = false;
 
     public static DataSave SerializeData(string jsonString)
     {
