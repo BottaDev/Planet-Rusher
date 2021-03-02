c4c8241b8b64194dc299824f1a942b031ece896c
Juli520 <50522986+Juli520@users.noreply.github.com>
2021-03-02 19:53:38 -0300

    Revert "Revert "Mini Bug fixed""
    This reverts commit 9ddba7374c959d1f5e482644073bd163166c287e.

--- Assets/Scripts/SpawnPowerUp.cs
+++ Assets/Scripts/SpawnPowerUp.cs
@@ -9,6 +9,7 @@ public class SpawnPowerUp : BaseSpawner
     public bool isSpawned = false;
     public bool isActived;
     public AudioSource audioSource;
+    public GameObject stopper;
     private UIManager uiManager;
 
     protected override void Start()
@@ -30,7 +31,23 @@ public class SpawnPowerUp : BaseSpawner
             isSpawned = true;
             uiManager.powerUpTimeLeftText.enabled = false;
             uiManager.powerUpSpawned.enabled = true;
-            SpawnPU();
+            PowerUpType();
+        }
+    }
+
+    private void PowerUpType()
+    {
+        int randomSpawn = Random.Range(1, 3);
+
+        switch (randomSpawn)
+        {
+            case 1:
+                SpawnStopper();
+                break;
+
+            case 2:
+                SpawnStopper();
+                break;
         }
     }
 
@@ -38,6 +55,14 @@ public class SpawnPowerUp : BaseSpawner
     {
         Vector3 position = new Vector3(Random.insideUnitSphere.x * sphereCollider.radius, Random.insideUnitSphere.y * sphereCollider.radius, Random.insideUnitSphere.z * sphereCollider.radius);
         Instantiate(objectToSpawn, position, new Quaternion(0, Random.value, 0, 0));
+        audioSource.Play();
+        currentSpawnTime = spawnTime;
+    }
+
+    private void SpawnStopper()
+    {
+        Vector3 position = new Vector3(Random.insideUnitSphere.x * sphereCollider.radius, Random.insideUnitSphere.y * sphereCollider.radius, Random.insideUnitSphere.z * sphereCollider.radius);
+        Instantiate(stopper, position, new Quaternion(0, Random.value, 0, 0));
         audioSource.Play();
         currentSpawnTime = spawnTime;
     }
